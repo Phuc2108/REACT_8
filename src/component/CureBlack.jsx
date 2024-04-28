8e5c11250c12bf4d7a9776db4e96e6b8860d7029
Phuc2108 <148455055+Phuc2108@users.noreply.github.com>
2024-04-28 07:05:46 +0000

    Hehe

--- src/component/CureBlack.jsx
+++ src/component/CureBlack.jsx
@@ -3,6 +3,6 @@ export default function CureBlack() {
   const girl = false
     return girl ? <h1>Cure Scarlet</h1> : <h1>Cure Lovely</h1>
 
-
+   
 
 } // ở đây sử dụng toán tử 3 ngôi gồm 1 vế hỏi và 2 vế 2 bên dấu 2 chấm
